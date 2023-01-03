72652a85d6ebacf830bc02d480f2da1e6bb02e5b
isorry123.eth <jones.spencer.us@gmail.com>
2023-01-03 16:18:38 -0500

    Update main.ts
    Firebase support

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBbv6hm45xJdxpHqKlHUElF0E0XRDz5w84",
+  authDomain: "pokewebkit.firebaseapp.com",
+  projectId: "pokewebkit",
+  storageBucket: "pokewebkit.appspot.com",
+  messagingSenderId: "604094832759",
+  appId: "1:604094832759:web:3bed43e220648223bee672",
+  measurementId: "G-1Y1G751MX5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
